fbb0d72a871c3c986f2df16a0f07a015e161280d
Cedric DENEUBOURG <deneubourg.cedric@thedigitalfab.com>
2020-05-01 00:16:26 +0200

    Add Migrations

--- db/migrate/20200430220931_create_doctors.rb
+++ db/migrate/20200430220931_create_doctors.rb
@@ -1,6 +1,9 @@
 class CreateDoctors < ActiveRecord::Migration[6.0]
   def change
     create_table :doctors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.belongs_to :city, index: true
 
       t.timestamps
     end
--- db/migrate/20200430221022_create_doctor_specialities.rb
+++ db/migrate/20200430221022_create_doctor_specialities.rb
@@ -1,7 +1,9 @@
 class CreateDoctorSpecialities < ActiveRecord::Migration[6.0]
   def change
     create_table :doctor_specialities do |t|
-
+      t.belongs_to :doctor, index: true
+      t.belongs_to :speciality, index: true
+      
       t.timestamps
     end
   end
